62d281a92c13a1269aa203be98dda0654cf6df28
Martin Emmert <martin@martinemmert.de>
2020-02-24 00:15:08 +0100

    feat: add TodoList component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,16 @@
 <script>
-	export let name;
+  import TodoList from "./TodoList.svelte";
+  const todos = [
+    { id: "1", todo: "create an app", done: true },
+    { id: "2", todo: "deploy via github to zeit", done: true },
+    { id: "3", todo: "reach world domination", done: false }
+  ];
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>My ToDo's</h1>
+  <h2>open:</h2>
+  <TodoList todos={todos.filter(item => !item.done)} />
+  <h2>archived:</h2>
+  <TodoList todos={todos.filter(item => item.done)} />
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ src/TodoList.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let todos;
+</script>
+
+{#if todos && todos.length}
+  <ul>
+    {#each todos as todo (todo.id)}
+      <li>{todo.todo}</li>
+    {/each}
+  </ul>
+{/if}
