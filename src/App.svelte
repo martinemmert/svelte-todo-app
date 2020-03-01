d0f2a20dc43a882aa9ca5a943fb1d94fc662e6ee
Martin Emmert <martin@martinemmert.de>
2020-03-01 12:31:18 +0100

    feat: display to-do items in a single list
    - removes seperate lists for completed and open items
    - completed items are filtered by default
    
    closes #15

--- src/App.svelte
+++ src/App.svelte
@@ -7,8 +7,5 @@
 <main>
   <h1>My ToDo's</h1>
   <AddTodoForm />
-  <h2>open</h2>
   <TodoList todos={$todos} filter={item => !item.done} />
-  <h2>completed</h2>
-  <TodoList todos={$todos} filter={item => item.done} />
 </main>
