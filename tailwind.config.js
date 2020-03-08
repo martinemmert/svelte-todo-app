62ca35684ed43da0cac9d38c3ccb37c2f20a4549
Martin Emmert <martin@martinemmert.de>
2020-03-08 17:15:44 +0100

    feat: add variants for colors, borders and scaling, add 0.5 spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,45 @@
-// tailwind.config.js
+const { colors } = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  plugins: [require("@tailwindcss/custom-forms")]
+  plugins: [require("@tailwindcss/custom-forms")],
+  theme: {
+    extend: {
+      spacing: {
+        "0.5": "0.125rem"
+      }
+    }
+  },
+  variants: {
+    backgroundColor: [
+      "responsive",
+      "group-hover",
+      "focus-within",
+      "focus",
+      "hover",
+      "active",
+      "visited",
+      "disabled"
+    ],
+    border: [
+      "responsive",
+      "group-hover",
+      "focus-within",
+      "focus",
+      "hover",
+      "active",
+      "visited",
+      "disabled"
+    ],
+    scale: ["responsive", "hover", "focus", "active", "group-hover"],
+    textColor: [
+      "responsive",
+      "group-hover",
+      "focus-within",
+      "focus",
+      "hover",
+      "active",
+      "visited",
+      "disabled"
+    ]
+  }
 };
