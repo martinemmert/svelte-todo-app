28d69008d72a3729f8176cfc2024b053730b4129
Martin Emmert <martin@martinemmert.de>
2020-03-14 19:20:16 +0100

    chore: add knobs storybook addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  stories: ["../src/**/*.stories.js"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-knobs"
+  ]
 };
