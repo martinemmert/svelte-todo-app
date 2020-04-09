e11639295348dd6977b6e370979657761867203e
Martin Emmert <martin@martinemmert.de>
2020-04-09 17:37:42 +0200

    fix: temporarily deactivate focus-within polyfill

--- postcss.config.js
+++ postcss.config.js
@@ -13,7 +13,14 @@ module.exports = ctx => ({
   plugins: [
     require("postcss-import"),
     require("tailwindcss"),
-    require("postcss-preset-env")({ stage: 1 }),
+    require("postcss-preset-env")({
+      stage: 1,
+      // FIXME: remove deactivation when the fix is applied
+      // https://github.com/jonathantneal/postcss-focus-within/issues/6
+      features: {
+        "focus-within-pseudo-class": false,
+      },
+    }),
     ...(ctx.env === "production" ? [purgecss, cssnano({ preset: "default" })] : []),
   ],
 });
